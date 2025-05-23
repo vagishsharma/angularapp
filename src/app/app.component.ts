735e11c3edcb75e888666ab826ce2586b0e1f547
vagishLocobuzz <vagish.sharma@locobuzz.com>
2025-05-23 16:43:30 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { FooterComponent } from './footer/footer.component';
+import { AppBodyComponent } from './app-body/app-body.component';
+import { HeaderComponent } from './header/header.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent,FooterComponent,AppBodyComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
   title = 'angular-app';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { AppComponent } from './app.component';
+import { AppBodyComponent } from './app-body/app-body.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+     { path: '', redirectTo: 'home', pathMatch: 'full'  },
+     { path: 'home', component: AppBodyComponent },   
+   
+    {
+        path: 'aboutme',
+        component: AboutMeComponent
+    }
+
+];

67e31acb1aef0913d910af5c17ece748a152f343
Vagish Sharma <vagishsharma@users.noreply.github.com>
2025-05-26 17:03:37 +0530

    Add files via upload

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-about-me',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.scss'
 })

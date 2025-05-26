be68a63f1bf9341390f70995a6bc7104349a4a53
Vagish Sharma <vagishsharma@users.noreply.github.com>
2025-05-26 16:56:58 +0530

    update files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,14 +2,26 @@ import { Routes } from '@angular/router';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { AppComponent } from './app.component';
 import { AppBodyComponent } from './app-body/app-body.component';
+import { ContactusComponent } from './contactus/contactus.component';
 
 export const routes: Routes = [
-     { path: '', redirectTo: 'home', pathMatch: 'full'  },
-     { path: 'home', component: AppBodyComponent },   
+     { 
+        path: '', 
+        redirectTo: 'home', 
+        pathMatch: 'full'  
+    },
+    { 
+        path: 'home', 
+        component: AppBodyComponent 
+    },   
    
     {
         path: 'aboutme',
         component: AboutMeComponent
+    },
+    {
+        path: 'contact-us',
+        component: ContactusComponent
     }
 
 ];
